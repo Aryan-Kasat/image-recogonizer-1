6e9449cfab5a1a46a3ab0ab1a000e71233cfd86d
Aryan-Kasat <72985192+Aryan-Kasat@users.noreply.github.com>
2021-09-04 11:49:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//https://teachablemachine.withgoogle.com/models/_ung3AVfq/
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 100
+});
+camera = document.getElementById("camera");
+Webcam.attach("camera");
+
+function takesnapshot() {
+    Webcam.snap(function (data_url) {
+        document.getElementById("result").innerHTML = "<img id='img1' src='" + data_url + "'>";
+    });
+}
+console.log("ml5.version", ml5.version)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_ung3AVfq/model.json", modelloaded)
+
+function modelloaded() {
+    console.log("modelloaded")
+}
+
+function check() {
+    img = document.getElementById("img1");
+    classifier.classify(img, getresult)
+}
+
+function getresult(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+        document.getElementById("result_object").innerHTML=result[0].label;
+        document.getElementById("result_accuracy").innerHTML=result[0].confidence.toFixed(3);
+    }
+}
